715eb05b1aa3a377f9429323c3968270d48963ca
ducky94tb <koolb0ypro94tb@gmail.com>
2023-03-09 23:06:34 +0700

    Add Hilt

---
+++ app/src/main/java/com/brouken/player/PlayerApplication.java
@@ -0,0 +1,9 @@
+package com.brouken.player;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class PlayerApplication extends Application {
+}
